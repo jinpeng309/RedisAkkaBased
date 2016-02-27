c6bc22a3bea04ede943eee89ff0d7caaed699a52
capslock <capslock1874@gmail.com>
2016-02-27 22:50:52 +0800

    some process for block list command, not complete

--- src/main/scala/com/capslock/redis/cache/record/ListRecord.scala
+++ src/main/scala/com/capslock/redis/cache/record/ListRecord.scala
@@ -1,22 +1,28 @@
 package com.capslock.redis.cache.record
 
-import java.util
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 
-import akka.actor.{Actor, ActorLogging, Stash}
+import akka.actor._
 import com.capslock.redis.command.list.ListCommand._
 import com.capslock.redis.command.response.ERROR_RESP
 import com.capslock.redis.command.response.RespCommand.{BULK_ARRAY_RESP_COMMAND, BULK_STRING_RESP_COMMAND, INTEGER_RESP_COMMAND}
 import com.capslock.redis.command.{ERROR_RESP_COMMAND, OK_RESP_COMMAND}
 import com.capslock.redis.utils.StringUtils
 
-import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration.FiniteDuration
 
 /**
   * Created by capsl on 2016/2/10.
   */
 class ListRecord extends Actor with ActorLogging with Stash {
   var listBuffer = ListBuffer.empty[String]
+  implicit val dispatcher = context.system.dispatcher
+
+  case class BlockClient(clientId: String, client: ActorRef, timer: Cancellable)
+
+  case class Timeout(clientId: String)
 
   private def removeElementByCountForward(list: ListBuffer[String], count: Int, value: String) = {
     var removeCount = count
@@ -95,22 +101,41 @@ class ListRecord extends Actor with ActorLogging with Stash {
           listBuffer = listBuffer.tail
           sender() ! BULK_ARRAY_RESP_COMMAND(List(key, value))
         case None =>
-          val client = sender()
-          var clientList = ListBuffer(client)
+          val uuid = UUID.randomUUID().toString
+          val recordActor: ActorRef = self
+          val cancel = context.system.scheduler.scheduleOnce(FiniteDuration(timeout.toLong, TimeUnit.SECONDS)) {
+            recordActor ! Timeout(uuid)
+          }
+          val client = BlockClient(uuid, sender(), cancel)
+          val blockClientList = ListBuffer(client)
           context.become({
             case BLPOP(_, t) =>
-              val blockClient = sender()
-              clientList.append(blockClient)
+              val uuid = UUID.randomUUID().toString
+              val cancel = context.system.scheduler.scheduleOnce(FiniteDuration(t.toLong, TimeUnit.SECONDS)) {
+                recordActor ! Timeout(uuid)
+              }
+              val blockClient = BlockClient(uuid, sender(), cancel)
+              blockClientList.append(blockClient)
+
             case LPUSH(_, values) =>
-              listBuffer.appendAll(values)
-              val listHead = listBuffer.head
-              listBuffer = listBuffer.dropRight(1)
-              clientList.head ! BULK_ARRAY_RESP_COMMAND(List(key, listHead))
-              clientList = clientList.tail
-              if (clientList.isEmpty) {
+              listBuffer.insertAll(0, values)
+              for (element <- listBuffer; blockClient <- blockClientList) {
+                listBuffer.remove(0)
+                blockClientList.remove(0)
+                blockClient.client ! BULK_ARRAY_RESP_COMMAND(List(key, element))
+              }
+              if (blockClientList.isEmpty) {
                 context.unbecome()
+              }
+            case Timeout(id) =>
+              blockClientList.find(blockClient => blockClient.clientId == id) match {
+                case Some(blockClient) =>
+                  blockClient.client ! BULK_STRING_RESP_COMMAND(None)
+                  blockClient.timer.cancel()
+                case _ =>
               }
           })
       }
+    case _ =>
   }
 }
